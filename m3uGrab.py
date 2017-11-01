29bc81b76c327ca9edb13a5fcc91078946069e25
Hezi Shahmoon <hshahmoon@apple.com>
2017-11-01 18:41:05 +0200

    Grab from master.m3u file
    Signed-off-by: Hezi Shahmoon <hshahmoon@apple.com>

---
+++ m3uGrab.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import re
+from urllib.request import urlopen
+import httplib2
+
+def grab(uri):
+    print(uri)
+    master = []
+    with open(uri[0],'r') as f:
+        master = f.readlines()
+    for line in master:
+        if line.startswith('http'):
+            search = re.search('http:/(?:/([^/]+))+',line)
+            filename = search[1].replace('.m3u8','.')+'mpeg'
+            with open(filename,'wb') as outFile:
+                child = urlopen(line).readlines()
+                for line in child:
+                    line = line.decode("utf-8")
+                    if line.startswith('http'):
+                        print(line)
+                        response = urlopen(line.replace('\n',''))
+                        outFile.write(response.read())
+
+
+
+if __name__ == "__main__":
+   grab(sys.argv[1:])
