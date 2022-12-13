89285ba6cb2cef82df30d3689b62d30c0b86c475
Hezi Shahmoon <hshahmoon@apple.com>
2022-12-13 16:23:31 +0200

    Update of grab algorithm according to Keshet Cast site
    Signed-off-by: Hezi Shahmoon <hshahmoon@apple.com>

--- m3uGrab.py
+++ m3uGrab.py
@@ -10,6 +10,7 @@ def grab(uri):
     with open(uri[0],'r') as f:
         master = f.readlines()
     for line in master:
+        preamble = line[:line.index(".mp4")+5]
         if line.startswith('http'):
             search = re.search('http.*?:/(?:/([^/]+))+',line)
             filename = search[1].replace('.m3u8','.')+'mpeg'
@@ -17,9 +18,9 @@ def grab(uri):
                 child = urlopen(line).readlines()
                 for line in child:
                     line = line.decode("utf-8")
-                    if line.startswith('http'):
+                    if line.startswith('media'):
                         print(line)
-                        response = urlopen(line.replace('\n',''))
+                        response = urlopen(preamble+line.replace('\n',''))
                         outFile.write(response.read())
 
 
